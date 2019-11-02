98a9795b68a0fcb99dbd62c0a9bcb3a09ca601b3
Miles Mickelson <milesmickelson87@gmail.com>
2019-11-01 20:32:58 -0700

    project complete with all 5 tests passsing and loving it

--- server/app.js
+++ server/app.js
@@ -3,6 +3,67 @@ const morgan = require('morgan');
 
 const app = express();
 
-// add your code here
+app.use(morgan('dev'));
+
+const initialData = [
+  {
+    todoItemId: 0,
+    name: 'an item',
+    priority: 3,
+    completed: false
+  },
+  {
+    todoItemId: 1,
+    name: 'another item',
+    priority: 2,
+    completed: false
+  },
+  {
+    todoItemId: 2,
+    name: 'a done item',
+    priority: 1,
+    completed: true
+  }
+];
+
+app.get('/', (req, res) => {
+  var status = {
+    status: 'ok'
+  }
+  res.status(200).send(status);
+});
+
+app.get('/api/TodoItems', (req, res) => {
+  res.status(200).send(initialData);
+});
+
+app.get('/api/TodoItems/:number', (req, res) => {
+  var itemId = req.params.number;
+  var reqItem = initialData[itemId];
+  res.status(200).send(reqItem);
+});
+
+app.post('/api/TodoItems/', (req, res) => {
+  var newItem = {
+    todoItemId: 0,
+    name: 'unique item',
+    priority: 1,
+    completed: false
+  }
+  res.status(201).send(newItem);
+});
+
+app.delete('/api/TodoItems/:number', (req, res) => {
+  var itemId = req.params.number;
+  var delItem = initialData[itemId];
+  if (delItem == itemId) {
+    delItem - itemId;
+  }
+  res.status(200).send(delItem);
+});
+
+app.get('*', function( req, res){
+  res.status(404).send('404 Error: Page not found');
+});
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
 const server = require('./app');
 
-// write your code here
+const PORT = process.env.PORT || 8484;
+
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
